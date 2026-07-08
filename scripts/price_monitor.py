2381bbbcc9c5d014bf88aae5412db5a127c7c41f
JMY枫月工作室 <jmy@example.com>
2026-07-08 09:29:27 +0800

    监控: 加入中持股份603903（7/7扫描），RSI78偏高观察中

--- scripts/price_monitor.py
+++ scripts/price_monitor.py
@@ -160,6 +160,18 @@ STOCKS = [
         "sl_pct": 8,
         "type": "观察"
     },
+    {
+        # 2026-07-08 从7/7扫描结果加入（扫描价15.82，涨+1.61% RSI71 MACD金叉）
+        "code": "1.603903",
+        "name": "中持股份",
+        "ts_code": "603903",
+        "cost": None,
+        "shares": 0,
+        "buy_date": None,
+        "tp_pct": 15,
+        "sl_pct": 8,
+        "type": "观察"
+    },
 ]
 
 STATE_FILE = "/home/jmy/.hermes/profiles/eastmoney-bot/.monitor_state.json"
@@ -220,7 +232,7 @@ def get_price(code):
 def get_rsi(code):
     """获取RSI"""
     try:
-        secid_map = {"0.002167": "0.002167", "0.159599": "0.159599", "1.518880": "1.518880", "1.513100": "1.513100", "0.000920": "0.000920", "1.600114": "1.600114", "0.002559": "0.002559", "1.603928": "1.603928", "1.600857": "1.600857"}
+        secid_map = {"0.002167": "0.002167", "0.159599": "0.159599", "1.518880": "1.518880", "1.513100": "1.513100", "0.000920": "0.000920", "1.600114": "1.600114", "0.002559": "0.002559", "1.603928": "1.603928", "1.600857": "1.600857", "1.603903": "1.603903"}
         secid = secid_map.get(code, code)
         url = f"http://push2his.eastmoney.com/api/qt/stock/kline/get?secid={secid}&fields1=f1,f2,f3,f4,f5,f6&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61&klt=101&fqt=1&end=20500101&lmt=30"
         req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
